437d9f54a873caa0219f3f7cdd5d81fe1976f9db
David Chamont <chamont@in2p3.fr>
2026-04-10 14:18:43 +0200

    Allow dpull with a turnkey image

--- bin/pull.sh
+++ bin/pull.sh
@@ -8,7 +8,7 @@ then
 fi
 cat_tmp=`cat /tmp/dev-scripts-recipe-dir-$PPID`
 if [[ ${cat_tmp} != /* ]] ; then
-  echo turnkey image: ${cat_tmp}
+  docker pull ${cat_tmp}
   exit
 fi
 export DEV_SCRIPTS_DOCKER_DIR=${cat_tmp}
